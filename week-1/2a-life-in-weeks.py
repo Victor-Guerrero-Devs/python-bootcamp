67e888ff3bef763df12191fd773a1c450cde62d8
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-08-11 21:14:06 -0500

    Add 2a-life-in-weeks.py

---
+++ week-1/2a-life-in-weeks.py
@@ -0,0 +1,6 @@
+age = input("What is your current age?")
+years = 90 - int(age)
+months = years * 12
+weeks = years * 52
+days = years * 365
+print(f"You have {days} days, {weeks} weeks, and {months} months left if you live until 90.")
