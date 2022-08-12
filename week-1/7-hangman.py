67002982290190596cc056f5602d3caca4620428
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-08-11 22:36:30 -0500

    Build input reflected on display

--- week-1/7-hangman.py
+++ week-1/7-hangman.py
@@ -3,16 +3,20 @@ import random
 words = ['root', 'sudo', 'way', 'back']
 
 chosen_word = random.choice(words)
-
+word_length = len(chosen_word)
 display = []
 
 for letter in chosen_word:
     display += '_'
 
+print(f"the answer is {chosen_word}")
+print(display)
+
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("right")
-    else:
-        print("wrong")
+        display[position] = letter
+
+print(display)
