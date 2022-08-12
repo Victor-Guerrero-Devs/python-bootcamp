6debba475b9bef79f6a11e866839a967e751505b
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-08-11 21:10:47 -0500

    Add 1-band-name-generator.py

---
+++ week-1/1-band-name-generator.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator!")
+
+city = input("Enter city of birth here: ")
+
+pet_name = input("Enter name of your pet here: ")
+
+print("Your new band name is " + city + " " + pet_name)
