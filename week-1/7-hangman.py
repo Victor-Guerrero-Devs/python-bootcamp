dc8f4b7a36832acfbc5753d1afbcd5ddabc094b3
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-08-11 22:48:54 -0500

    Finish hangman

--- week-1/7-hangman.py
+++ week-1/7-hangman.py
@@ -4,19 +4,34 @@ words = ['root', 'sudo', 'way', 'back']
 
 chosen_word = random.choice(words)
 word_length = len(chosen_word)
-display = []
 
+display = []
 for letter in chosen_word:
     display += '_'
 
 print(f"the answer is {chosen_word}")
 print(display)
 
-guess = input("Guess a letter: ").lower()
+lives = 6
+end_of_game = False
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You have {lives} lives left")
+        if lives == 0:
+            end_of_game = True
+            print("You lost")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
