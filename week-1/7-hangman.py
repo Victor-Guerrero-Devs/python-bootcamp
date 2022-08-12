ff1ccc546f7a63b9f7254baa9499b82452479c41
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-08-11 22:31:30 -0500

    Build guess input and checker

--- week-1/7-hangman.py
+++ week-1/7-hangman.py
@@ -8,3 +8,11 @@ display = []
 
 for letter in chosen_word:
     display += '_'
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("right")
+    else:
+        print("wrong")
