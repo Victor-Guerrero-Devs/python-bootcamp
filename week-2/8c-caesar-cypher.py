be438ceb929fe67626f89fae71026d08870714e9
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-08-12 22:17:30 -0500

    Fix alphabet out of range bug

--- week-2/8c-caesar-cypher.py
+++ week-2/8c-caesar-cypher.py
@@ -1,8 +1,12 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("type 'encode' to encrypt, type 'decode'  to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
 
 # create function that takes the text and shift as inputs
 def encrypt(plain_text, shift_amount):
